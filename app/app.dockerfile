893a82fbe4ab7772519d702e8717d582ea36099b
ducdv79 <duc.dv@beetechsoft.com>
2022-05-24 18:45:24 +0700

    react layout admin

--- app/app.dockerfile
+++ app/app.dockerfile
@@ -1,18 +1,12 @@
 # app.dockerfile
-FROM node:18-alpine as build-stage
+FROM node:18-alpine
 
 WORKDIR /app
-COPY package.json ./
+COPY package*.json ./
 
 RUN npm install
 
 COPY . .
 
-RUN npm ci
-
-ENV CI=true
-ENV PORT=3000
+EXPOSE 3000
 CMD [ "npm", "start" ]
-
-FROM build-stage AS build
-RUN npm run build
---
+++ app/src/_nav.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import CIcon from '@coreui/icons-react'
+import {
+  cilBell,
+  cilCalculator,
+  cilChartPie,
+  cilCursor,
+  cilDrop,
+  cilNotes,
+  cilPencil,
+  cilPuzzle,
+  cilSpeedometer,
+  cilStar,
+} from '@coreui/icons'
+import { CNavGroup, CNavItem, CNavTitle } from '@coreui/react'
+
+const _nav = [
+  {
+    component: CNavItem,
+    name: 'Dashboard',
+    to: '/dashboard',
+    icon: <CIcon icon={cilSpeedometer} customClassName="nav-icon" />,
+    badge: {
+      color: 'info',
+      text: 'NEW',
+    },
+  },
+  {
+    component: CNavTitle,
+    name: 'Components',
+  },
+  {
+    component: CNavGroup,
+    name: 'Base',
+    to: '/base',
+    icon: <CIcon icon={cilPuzzle} customClassName="nav-icon" />,
+    items: [
+      {
+        component: CNavItem,
+        name: 'Accordion',
+        to: '/base/accordion',
+      },
+      {
+        component: CNavItem,
+        name: 'Breadcrumb',
+        to: '/base/breadcrumbs',
+      },
+    ],
+  },
+]
+
+export default _nav
